82476340eae0727294c4fbf1231a7eb1545e1bb1
Sacha <sachamorgese@gmail.com>
2018-09-22 14:50:25 +0900

    Moved eslint, prettier and flow to root Using eslint-prettier to solve conflicts Changed registerServiceWorker to fix eslint issues Added pre-commit hook

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/api/anal", (req, res) => {
-  console.log("DAS PENES IST COMING!");
-  res.send("Just like Jesus wanted!");
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/anal', (req, res) => {
+  console.log('DAS PENES IST COMING!');
+  res.send('Just like Jesus wanted!');
 });
 
 const PORT = process.env.port || 3005;
-app.listen(PORT, () => console.log("Server ready"));
+app.listen(PORT, () => console.log('Server ready'));
