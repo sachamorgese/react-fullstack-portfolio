f43b7677d93c1bd76329abb6fdb2b4004737a9a5
Sacha <sachamorgese@gmail.com>
2018-09-23 03:09:53 +0900

    Google oauth now working

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const {
+  web: {
+    client_id: GOOGLE_CLIENT_ID,
+    client_secret: GOOGLE_CLIENT_SECRET,
+    redirect_uris: [googleCallbackURL],
+  },
+} = require('./config/client_id');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: googleCallbackURL,
+    },
+    (accessToken, refreshToken, profile, done) =>
+      console.log(accessToken, profile),
+  ),
+);
 
 const app = express();
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.get('/auth/oauth2callback', passport.authenticate('google'));
+
+app.get('/auth/logout', (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/anal', (req, res) => {
@@ -9,4 +44,4 @@ app.get('/api/anal', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => console.log('Server ready'));
+app.listen(PORT);
