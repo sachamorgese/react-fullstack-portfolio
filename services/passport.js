bf077deff6c8069717498c1ed140a8e34106fae5
Sacha Morgese <sachamorgese@gmail.com>
2018-09-28 00:06:01 +0900

    Add proxy to solve proxy issue

--- services/passport.js
+++ services/passport.js
@@ -25,6 +25,7 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: 'auth/google/callback',
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ google: { id: profile.id } }).then((existingUser) => {
