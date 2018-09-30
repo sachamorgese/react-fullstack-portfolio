c922e07a54749d10724a61a7bcf59d04ffd04435
Sacha <sachamorgese@gmail.com>
2018-09-30 17:04:22 +0900

    wip

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <div />
+    </BrowserRouter>
+  </div>
+);
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './index.css';
-import App from './App';
+import App from './components/App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = composeWithDevTools({
+  // Specify here name, actionsBlacklist, actionsCreators and other options if needed
+});
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware()));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
