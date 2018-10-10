85a440fc19fda6f1b2e10fbe3ad78d1950b9e9d4
Sacha Morgese <sachamorgese@gmail.com>
2018-10-11 00:07:48 +0900

    Added draft-js-plugins

--- client/src/components/Blog/NewPost/index.js
+++ client/src/components/Blog/NewPost/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
-
+import { EditorState, RichUtils, convertToRaw } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
+import 'draft-js-emoji-plugin/lib/plugin.css';
 import './index.css';
+
+const emojiPlugin = createEmojiPlugin();
+
+const { EmojiSuggestions } = emojiPlugin;
 
 export default class NewPost extends Component {
   constructor() {
@@ -12,11 +18,12 @@ export default class NewPost extends Component {
   }
 
   onChange = (editorState) => {
+    const contentState = editorState.getCurrentContent();
+    console.log('content state', convertToRaw(contentState));
     this.setState({ editorState });
   };
 
   handleKeyCommand = (command) => {
-    console.log(command);
     const { editorState } = this.state;
     const newState = RichUtils.handleKeyCommand(editorState, command);
 
@@ -67,7 +74,9 @@ export default class NewPost extends Component {
           editorState={editorState}
           onChange={this.onChange}
           handleKeyCommand={this.handleKeyCommand}
+          plugins={[emojiPlugin]}
         />
+        <EmojiSuggestions />
       </div>
     );
   }
