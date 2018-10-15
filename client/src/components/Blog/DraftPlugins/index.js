cfa2bb6e9dae958e6ab530357a8913d0a08c1a7a
Sacha Morgese <sacha.morgese@hde.co.jp>
2018-10-15 19:06:54 +0900

    Light refactoring, will probably amend

--- client/src/components/Blog/DraftPlugins/index.js
+++ client/src/components/Blog/DraftPlugins/index.js
@@ -2,39 +2,36 @@ import { RichUtils } from 'draft-js';
 
 const checkMetaAlt = (e) => e.metaKey && e.key === 'Alt';
 
-const createHighlighterPlugin = () => {
-  return {
-    customStyleMap: {
-      highlightBlue: {
-        background: 'blue',
-        padding: '0 .3em',
-        color: '#fff',
-      },
-      highlightYellow: {
-        background: 'yellow',
-        padding: '0 .3em',
-        color: '#fff',
-      },
+const createHighlighterPlugin = () => ({
+  customStyleMap: {
+    highlightBlue: {
+      background: 'blue',
+      padding: '0 .3em',
+      color: '#fff',
     },
-    keyBindingFn: (e) => {
-      if (checkMetaAlt(e) && e.key === 'b') {
-        e.preventDefault();
-        return 'highlight-blue';
-      }
+    highlightYellow: {
+      background: 'yellow',
+      padding: '0 .3em',
+      color: '#fff',
     },
-    handleKeyCommand: (command, editorState, { setEditorState }) => {
-      if (command === 'highlight-blue') {
-        setEditorState(
-          RichUtils.toggleInlineStyle(editorState, 'highlightBlue'),
-        );
-      } else if (command === 'highlight-yellow') {
-        setEditorState(
-          RichUtils.toggleInlineStyle(editorState, 'highlightYellow'),
-        );
-      }
-    },
-  };
-};
+  },
+  keyBindingFn: (e) => {
+    if (checkMetaAlt(e) && e.key === 'b') {
+      e.preventDefault();
+      return 'highlight-blue';
+    }
+    return '';
+  },
+  handleKeyCommand: (command, editorState, { setEditorState }) => {
+    if (command === 'highlight-blue') {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, 'highlightBlue'));
+    } else if (command === 'highlight-yellow') {
+      setEditorState(
+        RichUtils.toggleInlineStyle(editorState, 'highlightYellow'),
+      );
+    }
+  },
+});
 
 const temp = () => {};
 
