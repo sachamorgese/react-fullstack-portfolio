931b6612c4153cce4d6a9df5a9488cf6f046be94
Sacha Morgese <sachamorgese@gmail.com>
2019-09-27 21:59:04 +0100

    Added .nvmrc to set node version to 12 automatically Updated node vs in .travis.yml
    Todo: Authentication on the client
    Todo: Edit Post
    Todo: Add Loading
    Todo: Display posts for visitors

--- client/src/components/Blog/index.js
+++ client/src/components/Blog/index.js
@@ -1,10 +1,15 @@
+// @flow
+
 import React from 'react';
 import { Route } from 'react-router-dom';
+import type { matchPath } from 'react-router-dom'
+
 import Draft from './Draft';
 import AdminHome from './AdminHome';
 import BlogPost from './BlogPost';
 
-const Blog = ({ match }) => (
+
+const Blog = ({ match }: {match: matchPath}) => (
   <>
     <Route path={`${match.path}admin`} component={AdminHome} />
     <Route path={`${match.path}draft/:id`} component={Draft} />
