c09d9aee46bb3d59e629f79e24289c4ed88f7a31
Sacha Morgese <sachamorgese@gmail.com>
2019-12-01 23:46:32 +0000

    Added necessary steps for production on index.js Removed build and added heroku-postbuild
    Todo: Fix UI
    Todo: Add Loading
    Todo: Display posts for visitors

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookies = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys'); // eslint-disable-line import/no-unresolved
+const keys = require('./config/keys');
 require('./models');
 
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true, useUnifiedTopology: true },
-  // eslint-disable-next-line no-console,flowtype/require-parameter-type,flowtype/require-return-type
+  // eslint-disable-next-line flowtype/require-parameter-type,flowtype/require-return-type
   (error) => console.log(error),
 );
 
@@ -32,6 +32,23 @@ app.use(express.json());
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
 require('./routes/userRoutes')(app);
+
+console.log(process.env)
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  // eslint-disable-next-line global-require
+  const path = require('path');
+  // eslint-disable-next-line flowtype/require-parameter-type,flowtype/require-return-type
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT);
